af6aa9114de54e4eb2984ea6f0da0546fbc37cee
Fsesnick <61622322+Fsesnick@users.noreply.github.com>
2021-03-22 15:39:05 -0300

    Deploy to GitHub Pages

---
+++ build/precache-manifest.3ddef58231f66da8ff9989c03442538e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9c149b2f91a63baf2573",
+    "url": "/course-directory/static/css/main.77a0b72a.chunk.css"
+  },
+  {
+    "revision": "9c149b2f91a63baf2573",
+    "url": "/course-directory/static/js/main.9c149b2f.chunk.js"
+  },
+  {
+    "revision": "25a97381f2fd20d18f35",
+    "url": "/course-directory/static/js/1.25a97381.chunk.js"
+  },
+  {
+    "revision": "527751acb6022af06d5d",
+    "url": "/course-directory/static/js/runtime~main.527751ac.js"
+  },
+  {
+    "revision": "841b1a2fd898b0b6244e9dbee72e75b7",
+    "url": "/course-directory/index.html"
+  }
+];
